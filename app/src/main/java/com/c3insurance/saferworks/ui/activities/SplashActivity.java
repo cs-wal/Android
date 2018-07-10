70406edab3471e1b239b1c1147f1a12637bb327c
Naresh <naresh.b@westagilelabs.com>
2018-07-10 14:13:10 +0530

    Initial project checkin

---
+++ app/src/main/java/com/c3insurance/saferworks/ui/activities/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.c3insurance.saferworks.ui.activities;
+
+import android.os.Bundle;
+
+import com.c3insurance.saferworks.R;
+import com.c3insurance.saferworks.ui.base.BaseActivity;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
---
+++ app/src/main/java/com/c3insurance/saferworks/ui/base/BaseActivity.java
@@ -0,0 +1,7 @@
+package com.c3insurance.saferworks.ui.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+}
